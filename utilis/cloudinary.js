ef5def09834d36efb2ece270dd277e50e8032bf8
determinedProgrammer <isahabdulwahab@gmail.com>
2024-09-08 18:26:46 -0700

    ...

--- utilis/cloudinary.js
+++ utilis/cloudinary.js
@@ -1,17 +1,11 @@
-//require('dotenv').config(); 
+require('dotenv').config(); 
 
  const cloudinary = require('cloudinary').v2;
 
 
-// cloudinary.config({
-//   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-//   api_key: process.env.CLOUDINARY_API_KEY,
-//   api_secret: process.env.CLOUDINARY_API_SECRET
-// });
-// console.log(process.env.CLOUDINARY_API_SECRET,)
 cloudinary.config({
-    cloud_name: 'dgznlvgur',
-    api_key: '719286164184721',
-    api_secret: 'TVmIi1u_UG5qAPzwbpp5BrkyBak'
-  });
-  
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
+});
+console.log(process.env.CLOUDINARY_API_SECRET,)
